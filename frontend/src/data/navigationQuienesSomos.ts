577d90317f0f35e00abc3b36a3f36171c724f8a0
SieghartSantiago <sieghartsantiago@gmail.com>
2026-06-29 14:05:32 -0300

    Subnavegacion de la seccion de quienes somos

---
+++ frontend/src/data/navigationQuienesSomos.ts
@@ -0,0 +1,16 @@
+import type Navigation from './interfaces/Navigation'
+
+export const navigationQuienesSomos: Navigation[] = [
+  {
+    name: 'Historia',
+    href: '/quienes-somos/historia',
+  },
+  {
+    name: 'Equipo',
+    href: '/quienes-somos/equipo',
+  },
+  {
+    name: 'Logros',
+    href: '/quienes-somos/logros',
+  },
+]
