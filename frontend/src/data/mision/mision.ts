edd02b1a914667b0540991da7b0043b63ed5f793
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-03 18:16:33 -0300

    Sección misión

---
+++ frontend/src/data/mision/mision.ts
@@ -0,0 +1,40 @@
+import type Mision from '../interfaces/Mision'
+
+export const misiones: Mision[] = [
+  {
+    titulo: 'Conciencia pública',
+    contenido:
+      'Incentivar una mayor conciencia pública de los métodos preventivos, diagnósticos y terapéuticos de los cánceres oncológicos',
+    img: `icon-publico.svg`,
+  },
+  {
+    titulo: 'Calidad de atención',
+    contenido:
+      'Promover el mejoramiento en la calidad de atención en los centros asistenciales de todo el país',
+    img: 'icon-cuidado.svg',
+  },
+  {
+    titulo: 'Cuidados psicosociales',
+    contenido:
+      'Atender las necesidades psicosociales del niño enfermo de cáncer y su familia a lo largo de las distintas etapas de la enfermedad',
+    img: 'icon-mental.svg',
+  },
+  {
+    titulo: 'Acceso de recursos',
+    contenido:
+      'Facilitar el acceso a los recursos que permiten la continuidad del tratamiento en tiempo y forma',
+    img: 'icon-medical.svg',
+  },
+  {
+    titulo: 'Información',
+    contenido:
+      'Ofrecer información de utilidad acerca de distintos aspectos relacionados con la enfermedad y su tratamiento',
+    img: 'icon-information.svg',
+  },
+  {
+    titulo: 'Defensa de derechos',
+    contenido:
+      'Defender los derechos del niño enfermo de cáncer y trabajar conjuntamente con quiénes están a cargo de las legislaciones para lograr un proyecto de ley que los ampare en el periodo del tratamiento',
+    img: 'icon-derecho.svg',
+  },
+]
