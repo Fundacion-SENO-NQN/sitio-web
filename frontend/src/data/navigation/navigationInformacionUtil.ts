d96ca27d45b1ab67407fed5423f709c6112100a8
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-03 18:19:34 -0300

    Normalización de las secciones de navegación

---
+++ frontend/src/data/navigation/navigationInformacionUtil.ts
@@ -0,0 +1,20 @@
+import type Navigation from '../interfaces/Navigation'
+
+export const navigationInformacionUtil: Navigation[] = [
+  {
+    name: 'Como recibir ayuda',
+    href: '/necesito-ayuda',
+  },
+  {
+    name: 'Como ayudar',
+    href: '/quiero-ayudar',
+  },
+  {
+    name: 'Donde estamos',
+    href: '#',
+  },
+  {
+    name: 'Donar cosas',
+    href: '/quiero-ayudar/donar-cosas',
+  },
+]
