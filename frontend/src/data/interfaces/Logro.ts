701fc804b7cd0d69744a2f85cba413ff7efe962a
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-04 17:00:44 -0300

    Pagina logros

---
+++ frontend/src/data/interfaces/Logro.ts
@@ -0,0 +1,5 @@
+export default interface Logro {
+  titulo: string
+  contenido: string
+  img: string
+}
---
+++ frontend/src/data/logros/logros.ts
@@ -0,0 +1,9 @@
+import type Logro from '../interfaces/Logro'
+
+export const logros: Logro[] = [
+  {
+    titulo: 'Escalera de acceso al primer piso',
+    contenido: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati perferendis ut harum amet neque quidem, nisi, dolore eum beatae saepe earum illum cupiditate fugiat, temporibus iste labore accusamus at ipsum.',
+    img: 'logros1',
+  },
+]
