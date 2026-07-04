cffe349395456ede9ad6253037924cb06281f428
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-04 19:14:52 -0300

    Navegacion de la seccion pelucas

---
+++ frontend/src/data/navigation/navigationPelucas.ts
@@ -0,0 +1,16 @@
+import type Navigation from '../interfaces/Navigation'
+
+export const navigationPelucas: Navigation[] = [
+  {
+    name: 'Cuidado',
+    href: '/pelucas/cuidado',
+  },
+  {
+    name: 'Taller',
+    href: '/pelucas/taller',
+  },
+  {
+    name: 'Donar pelo',
+    href: '/quiero-ayudar/donar-pelo',
+  },
+]
