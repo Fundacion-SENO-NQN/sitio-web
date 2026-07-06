4e4d9843a50b49570ba7ea8466b4e582753b9ec0
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-06 10:11:45 -0300

    Sección Quiero ayudar

---
+++ frontend/src/data/navigation/navigationQuieroAyudar.ts
@@ -0,0 +1,24 @@
+import type Navigation from '../interfaces/Navigation'
+
+export const navigationQuieroAyudar: Navigation[] = [
+  {
+    name: 'Donar',
+    href: '/quiero-ayudar/donar',
+  },
+  {
+    name: 'Donar cosas',
+    href: '/quiero-ayudar/donar-cosas',
+  },
+  {
+    name: 'Donar pelo',
+    href: '/quiero-ayudar/donar-pelo',
+  },
+  {
+    name: 'Empresas',
+    href: '/quiero-ayudar/empresas',
+  },
+  {
+    name: 'Voluntariado',
+    href: '/quiero-ayudar/quiero-ser-voluntario',
+  },
+]
---
+++ frontend/src/data/quieroAyudar/quieroAyudar.ts
@@ -0,0 +1,29 @@
+import type QuieroAyudar from '../interfaces/QuieroAyudar'
+
+export const quieroAyudar: QuieroAyudar[] = [
+  {
+    titulo: 'Donar',
+    img: 'icon-dinero',
+    url: '/quiero-ayudar/donar',
+  },
+  {
+    titulo: 'Donar cosas',
+    img: 'icon-caja',
+    url: '/quiero-ayudar/donar-cosas',
+  },
+  {
+    titulo: 'Donar pelo',
+    img: 'icon-pelo',
+    url: '/quiero-ayudar/donar-pelo',
+  },
+  {
+    titulo: 'Empresa (sponsor)',
+    img: 'icon-empresa',
+    url: '/quiero-ayudar/empresas',
+  },
+  {
+    titulo: 'Voluntariado',
+    img: 'icon-voluntariado',
+    url: '/quiero-ayudar/quiero-ser-voluntario',
+  },
+]
