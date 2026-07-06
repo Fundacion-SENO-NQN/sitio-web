93fe69865fbf954feaacf85f0cc21452345520ff
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-06 10:15:52 -0300

    Eliminación del .svg en las imagenes (un factor común)

--- frontend/src/data/mision/mision.ts
+++ frontend/src/data/mision/mision.ts
@@ -5,36 +5,36 @@ export const misiones: Mision[] = [
     titulo: 'Conciencia pública',
     contenido:
       'Incentivar una mayor conciencia pública de los métodos preventivos, diagnósticos y terapéuticos de los cánceres oncológicos',
-    img: `icon-publico.svg`,
+    img: `icon-publico`,
   },
   {
     titulo: 'Calidad de atención',
     contenido:
       'Promover el mejoramiento en la calidad de atención en los centros asistenciales de todo el país',
-    img: 'icon-cuidado.svg',
+    img: 'icon-cuidado',
   },
   {
     titulo: 'Cuidados psicosociales',
     contenido:
       'Atender las necesidades psicosociales del niño enfermo de cáncer y su familia a lo largo de las distintas etapas de la enfermedad',
-    img: 'icon-mental.svg',
+    img: 'icon-mental',
   },
   {
     titulo: 'Acceso de recursos',
     contenido:
       'Facilitar el acceso a los recursos que permiten la continuidad del tratamiento en tiempo y forma',
-    img: 'icon-medical.svg',
+    img: 'icon-medical',
   },
   {
     titulo: 'Información',
     contenido:
       'Ofrecer información de utilidad acerca de distintos aspectos relacionados con la enfermedad y su tratamiento',
-    img: 'icon-information.svg',
+    img: 'icon-information',
   },
   {
     titulo: 'Defensa de derechos',
     contenido:
       'Defender los derechos del niño enfermo de cáncer y trabajar conjuntamente con quiénes están a cargo de las legislaciones para lograr un proyecto de ley que los ampare en el periodo del tratamiento',
-    img: 'icon-derecho.svg',
+    img: 'icon-derecho',
   },
 ]
