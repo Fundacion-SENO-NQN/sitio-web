5f72f9f0c6782ab45c9881decf6429a7a55ea47e
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-13 17:20:39 -0300

    Interfaz y data de los metodos de donacion

---
+++ frontend/src/data/donacion/mediosPagoBPN.ts
@@ -0,0 +1,8 @@
+import type MetodoPago from '../interfaces/MetodoPago'
+
+export const mediosPagoBPN: MetodoPago[] = [
+  {
+    titulo: 'CBU',
+    valor: '0970099413004665390014',
+  },
+]
---
+++ frontend/src/data/donacion/mediosPagoMP.ts
@@ -0,0 +1,12 @@
+import type MetodoPago from '../interfaces/MetodoPago'
+
+export const mediosPagoMP: MetodoPago[] = [
+  {
+    titulo: 'Alias',
+    valor: 'fundacionseno',
+  },
+  {
+    titulo: 'CVU',
+    valor: '0000003100085015012040',
+  },
+]
