5d6a2e29991023add0cde9477b4f6e8dd859c24a
SieghartSantiago <sieghartsantiago@gmail.com>
2026-07-13 17:22:01 -0300

    Interfaz y data de noticias

---
+++ frontend/src/data/noticia/noticia.ts
@@ -0,0 +1,39 @@
+import type Noticia from '../interfaces/Noticia'
+
+export const noticia: Noticia[] = [
+  {
+    titulo: 'Noticia 1',
+    fecha: '7 de julio de 2026',
+    content:
+      'Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1',
+    img: 'not1',
+  },
+  {
+    titulo: 'Noticia 2',
+    fecha: '7 de julio de 2026',
+    content:
+      'Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1',
+    img: 'not1',
+  },
+  {
+    titulo: 'Noticia 3',
+    fecha: '7 de julio de 2026',
+    content:
+      'Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1',
+    img: 'not1',
+  },
+  {
+    titulo: 'Noticia 4',
+    fecha: '7 de julio de 2026',
+    content:
+      'Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1',
+    img: 'not1',
+  },
+  {
+    titulo: 'Noticia 5',
+    fecha: '7 de julio de 2026',
+    content:
+      'Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1,Contenido de la noticia 1',
+    img: 'not1',
+  },
+]
