310b7960d7d643f20e580898031b597b1af00089
SieghartSantiago <sieghartsantiago@gmail.com>
2026-08-14 15:07:01 -0300

    Cambios de accesibilidad y seguridad

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -17,6 +17,22 @@ export default defineConfig({
     inlineStylesheets: 'always'
   },
 
+  security: {
+    csp: {
+      directives: [
+        "default-src 'self'",
+
+        "img-src 'self' data: blob: https://pub-508ef05ca2d548c1b336a8b1f0f31c83.r2.dev https://img.fundacionseno.org",
+
+        "font-src 'self' data:",
+
+        "connect-src 'self' https://sitio-web-fundacion-seno.fly.dev https://api.fundacionseno.org",
+
+        "frame-src 'self' https://www.google.com https://maps.google.com"
+      ]
+    }
+  },
+
   integrations: [
     sitemap({
       filter: (page) =>
